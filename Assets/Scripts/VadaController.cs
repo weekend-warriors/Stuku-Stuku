fe7e5cec98e8b095ebfaad12afb5665778c51319
Aleksandras Trakas <pastasaleksui@gmail.com>
2021-01-30 22:20:06 +0200

    Vada v1

--- Assets/Scripts/VadaController.cs
+++ Assets/Scripts/VadaController.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class VadaController : MonoBehaviour
+public class VadaController : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    public Material VadaColor;
+    public GameObject[] PaintedParts;
+    private bool isBlind = true;
+    public float BlindPeriod;
+    private PlayerController playerController;
+    private List<GameObject> TaggedRunners;
+
     void Start()
     {
-        
+        playerController = GetComponent<PlayerController>();
+        foreach(GameObject obj in PaintedParts)
+        {
+            obj.GetComponent<MeshRenderer>().material = VadaColor;
+        }
+
+        if (isLocalPlayer)
+        {
+            StartCoroutine(Deblind(BlindPeriod));
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isBlind)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ClickRaycast = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ClickRaycast, out RaycastHit ClickedObject))
+            {
+                if (ClickedObject.collider.CompareTag("Player"))
+                {
+                    TaggedRunners.Add(ClickedObject.collider.gameObject);
+                    // Maybe outline ClickedObject
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isBlind && other.CompareTag("FlagName")) // Need to tag flag
+        {
+            for(int i = TaggedRunners.Count; i >= 0; i--)
+            {
+                // Stuck player i
+                TaggedRunners.RemoveAt(i);
+            }
+        }
+    }
+
+    IEnumerator Deblind(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        isBlind = false;
     }
 }
